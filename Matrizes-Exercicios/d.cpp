f2d0d98e96a2cd945b16b5fd0232c08898b5cd12
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-06-07 15:19:02 -0300

    Update d.cpp

--- Matrizes-Exercicios/d.cpp
+++ Matrizes-Exercicios/d.cpp
@@ -8,13 +8,13 @@ int main()
         cout << "Digite o " << i + 1 << "º " << " número: ";
         cin >> a[i];
         c[i][0] = a[i] + 5;
-       long long fat = 1;
+        long long fat = 1;
         for (int j = 1; j <= a[i]; j++)
         {
             fat *= j;
         }
         c[i][1] = fat;
-  
+
         c[i][2] = a[i] * a[i];
     }
     for (int i = 0; i < 10; i++)

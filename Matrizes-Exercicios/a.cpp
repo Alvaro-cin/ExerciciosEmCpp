d20434af745ea53d6ca2f60796528eb70274bf72
Francisco Álvaro Chaves da Silva <alvaro.chaves@escolar.ifrn.edu.br>
2025-06-20 11:02:26 -0300

    C++

--- Matrizes-Exercicios/a.cpp
+++ Matrizes-Exercicios/a.cpp
@@ -27,9 +27,8 @@ int main()
         for (int j = 0; j < 3; j++)
         {
             c[i][j] = a[i][j] + b[i][j];
-             cout << c[i][j] << " ";
+            cout << c[i][j] << " ";
         }
-       cout << endl;
+        cout << endl;
     }
-
 }
